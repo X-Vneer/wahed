8fb3f8ec052d12bcd0945de8a107de6045c7a240
Moneer .A Mizyed <xv.neer.business@gmail.com>
2026-01-02 23:33:47 +0200

    Refactor Providers component and remove session store implementation
    - Simplified the Providers component by removing the SessionStoreProvider and access token retrieval, streamlining the component's structure.
    - Deleted the session store and its provider, as they are no longer utilized in the application, reducing code complexity and improving maintainability.

--- components/providers/index.tsx
+++ components/providers/index.tsx
@@ -1,21 +1,12 @@
 import React from "react"
 import { QueryProvider } from "./query-provider"
-import { SessionStoreProvider } from "@/stores/session-store/session-store-provider"
-import { getAccessToken } from "@/lib/get-access-token"
 
 type Props = {
   children: React.ReactNode
 }
 
 const Providers = async ({ children }: Props) => {
-  const accessToken = await getAccessToken()
-  return (
-    <QueryProvider>
-      <SessionStoreProvider accessToken={accessToken}>
-        {children}
-      </SessionStoreProvider>
-    </QueryProvider>
-  )
+  return <QueryProvider>{children}</QueryProvider>
 }
 
 export default Providers
