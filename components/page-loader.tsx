f89ac56e11a386d154779fb8f77b7630da0f99e2
Moneer .A Mizyed <xv.neer.business@gmail.com>
2026-02-16 20:35:03 +0200

    Refactor PageLoader component for improved loading UI
    - Renamed PageLeader to PageLoader for consistency and clarity.
    - Enhanced the PageLoader component structure with a flexbox layout and styling for better visual presentation during loading states.

--- components/page-loader.tsx
+++ components/page-loader.tsx
@@ -1,8 +1,13 @@
-import React from "react"
 import { Spinner } from "./ui/spinner"
 
-const PageLeader = () => {
-  return <PageLoader />
+const PageLoader = () => {
+  return (
+    <div className="flex flex-col rounded-md bg-white p-2">
+      <div className="flex min-h-[calc(100svh-100px)] items-center justify-center">
+        <Spinner className="text-muted-foreground size-8" />
+      </div>
+    </div>
+  )
 }
 
-export default PageLeader
+export default PageLoader
